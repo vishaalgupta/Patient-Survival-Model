aed471a0bc39614854d04ba6baef1f1f25552b6e
isaacrosenthal06 <isaacrosenthal06@gmail.com>
2022-08-11 22:26:29 -0500

    Updates app.py

--- app.py
+++ app.py
@@ -1,36 +1,50 @@
-# import necessary libraries
-from models import create_classes
-import os
-from flask import (
-    Flask,
-    render_template,
-    jsonify,
-    request,
-    redirect)
+from flask import Flask, render_template, redirect, jsonify
+import json
+import numpy as np
+import pandas as pd
+from bson import json_util
 
-#################################################
-# Flask Setup
-#################################################
 app = Flask(__name__)
 
 
 
-# create route that renders index.html template
+##Need to add connection to model in line below
+##model = load(open('./models/model_linregress_2022081800.pkl', 'rb'))
+
 @app.route("/")
 def home():
-    return render_template("index.html", )
+    prediction_text = ""
+    eturn render_template("index.html", result = prediction_text)
 
 
-@app.route("/send", methods=["GET", "POST"])
+@app.route("/send", methods=["POST"])
 def send():
-    if request.method == "POST":
-        name = request.form["petName"]
-        lat = request.form["petLat"]
-        lon = request.form["petLon"]
+    icu_death_prob = request.form["icu"]
+    ventilation = request.form["ventilation"]
+    gcs_verbal = request.form["gscVerbal"]
+    gcs_eye = request.form["gscEyes"]
+    hos_death_prob = request.form['hospital']
+    gcs_motor = request.form['gscMotor']
+    min_sys_ni = request.form['bpNon']
+    min_sis = request.form['bp']
+    age = request.form['age']
+    min_mean_bp = request.form['minBP']
 
-        return redirect("/", code=302)
+    final_features = [np.array([icu_death_prob, ventilation, gcs_verbal, gcs_eye, hos_death_prob, gcs_motor, min_sys_ni, min_sis, age, min_mean_bp])]
 
-    return render_template("form.html")
+    # use form results to make prediction
+    prediction = model.predict(final_features)[0]
+
+    # create html content - either single variable, dictionary, or string
+    prediction_text = f"The survival prediction is: prediction."
+
+    # send prediction to html page
+    return render_template("index.html", result = prediction_text)
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
 
 
