462256d9e1736d0777957edfd490d9d5ff7912aa
isaacrosenthal06 <isaacrosenthal06@gmail.com>
2022-08-13 11:13:32 -0500

    app.py changes

--- app.py
+++ app.py
@@ -1,42 +1,51 @@
-from flask import Flask, render_template, redirect, jsonify
+from flask import Flask, render_template, redirect, jsonify, request
 import json
 import numpy as np
 import pandas as pd
-from bson import json_util
+from joblib import dump, load
+
+
 
 app = Flask(__name__)
 
 
 
 ##Need to add connection to model in line below
-##model = load(open('./models/model_linregress_2022081800.pkl', 'rb'))
+model = load('ML-models/model_ExtraTrees.joblib')
 
 @app.route("/")
 def home():
     prediction_text = ""
-    eturn render_template("index.html", result = prediction_text)
+    return render_template("index.html", result = prediction_text)
 
 
 @app.route("/send", methods=["POST"])
 def send():
-    icu_death_prob = request.form["icu"]
-    ventilation = request.form["ventilation"]
-    gcs_verbal = request.form["gscVerbal"]
-    gcs_eye = request.form["gscEyes"]
-    hos_death_prob = request.form['hospital']
-    gcs_motor = request.form['gscMotor']
-    min_sys_ni = request.form['bpNon']
-    min_sis = request.form['bp']
-    age = request.form['age']
-    min_mean_bp = request.form['minBP']
+    icu_death_prob = float(request.form["icu"])
+    ventilation = float(request.form["ventilation"])
+    gcs_verbal = float(request.form["gscVerbal"])
+    gcs_eye = float(request.form["gscEyes"])
+    hos_death_prob = float(request.form['hospital'])
+    gcs_motor = float(request.form['gscMotor'])
+    min_sys_ni = float(request.form['bpNon'])
+    min_sis = float(request.form['bp'])
+    age = float(request.form['age'])
+    min_mean_bp = float(request.form['minBP'])
 
     final_features = [np.array([icu_death_prob, ventilation, gcs_verbal, gcs_eye, hos_death_prob, gcs_motor, min_sys_ni, min_sis, age, min_mean_bp])]
 
     # use form results to make prediction
     prediction = model.predict(final_features)[0]
 
+    text =""
+
+    if prediction == 1:
+        text = "Patient is not predicted to survive"
+    else:
+        text = "Patient is predicted to survive"
+
     # create html content - either single variable, dictionary, or string
-    prediction_text = f"The survival prediction is: prediction."
+    prediction_text = f"The survival prediction is: {text}."
 
     # send prediction to html page
     return render_template("index.html", result = prediction_text)
